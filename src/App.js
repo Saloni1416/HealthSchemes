280e0d23d9c3580970791239b33a9e48d9bf1e84
Saloni_A_Fernandes <71886464+Saloni1416@users.noreply.github.com>
2022-05-10 18:31:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "./index.css"
+import s1 from "./s1"
+import s2 from "./s2"
+import s3 from "./s3"
+
+function App() {
+  return (
+    <div className="App">
+      <s1/>
+      <s2/>
+      <s3/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/s2.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+function s2
+() {
+  return (
+    <div>
+    <h2>2)Aam Admi Bhima Yojana:</h2>
+    <img src={process.env.PUBLIC_URL+"aaby.jpg"} alt="Ayushman Bharat" className='ay' />
+    <em><p>What is this Scheme?</p></em>
+    <p>Aam Aadmi Bima Yojana is a Social Security Scheme which had been initiated as well as administered by the Indian Government for mostly the rural landless households. It was launched as recently as October 2, 2007. </p>
+
+    <p>The Aam Aadmi Bima Yojana covers natural death/accidental death, permanent total disability, and partial permanent disability. This scheme provides coverage to the head of the family or one earning member in the family. The premium of <strong>Rs.200/-</strong> per person per annum is shared equally by the Central Government and the State Government, so as to ensure that the insured person has to pay no premium.</p>
+    <em><p>The following are the basic requirements for applying for the Aam Aadmi Bima Yojana:</p></em>
+    
+    
+    
+    <ol>
+        <li>The applicants should be aged between 18 and 59.</li>
+        <li>The applicant ideally should be the head of the family or one of the earning members of a below poverty line family (BPL) or marginally above the poverty line under identified vocational group/rural landless household.</li>
+        
+    </ol>
+
+    <em><p>The Documents Required For Enrolling in the Aam Aadmi Bima Yojana Are:</p></em>
+    <ol>
+        <li>Aadhar Card of the applicant</li>
+        <li>Government-issued Identity Card</li>
+        <li>Ration card</li>
+        <li>Birth certificate</li>
+        <li>School certificate evidence</li>
+        <li>Voter id</li>
+        <li>Mobile number</li>
+        <li>Passport size photo</li>
+        <li>Caste Certificate</li>
+    </ol>
+    <p><em>Benefits of Aam Aadmi Bhima Yojana</em></p>
+    <p>1.The Death Benefit Of this scheme is given to the surviving nominees or family of the policyholder on the event of a natural death is <strong>Rs. 30,000/-</strong></p>
+    <p>2.In case the death has occurred due to accident/on permanent total disability due to accident (loss of 2 eyes or 2 limbs) the amount given to the nominees or family members of the owner of the policy is <strong>Rs. 75,000.</strong></p>
+    <p>3.In the case of partial permanent disability due to accident (loss of one eye or one limb) the amount given to the nominees or family members of the owner of the policy is <strong>Rs. 37500/-</strong> </p>
+    <p>4.Scholarship benefits: Scholarship is a Free Add-on benefit provided to a maximum of two children of the beneficiary who are studying between 9th to 12th Standard @ Rs.100/= per month for each child, which is payable at a half yearly basis – July 1 and on January 1, every year.</p>
+
+    <em><p>How to apply online for Aam admi bima yojana?</p></em>
+    <ol>
+        <li> Firstly, go to the official website of the LIC,<em> <a href="https://licindia.in/" target="_blank">Click here to Apply</a></em> </li>
+        <li>The homepage of the website will show on the screen.</li>
+        <li>.Select the option “LIC Aam Aadmi Bima Yojana” given at the homepage of the website.</li>
+        <li>An application form will show up on your screen.</li>
+        <li>.Fill the mandatory details asked in the application form such as Personal details, Address details, Income details, etc.</li>
+        <li>Upload the necessary documents asked.</li>
+        <li>Select the Submit option and your application form is submitted.</li>
+    </ol>
+    <hr/>
+
+    
+    </div>
+  )
+}
+
+export default s2
