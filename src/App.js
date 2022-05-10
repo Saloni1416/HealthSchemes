676dd6a5f76ddf12f0a6434ff8d3d24ad2f4682c
Saloni_A_Fernandes <71886464+Saloni1416@users.noreply.github.com>
2022-05-10 18:42:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import styles from "./index.css"
-import s1 from "./s1"
-import s2 from "./s2"
-import s3 from "./s3"
+
 
 function App() {
   return (
     <div className="App">
-      <s1/>
-      <s2/>
-      <s3/>
+     
     </div>
   );
 }
